fc71bbad350e8b6a81d38edc6223973bff4038cb
neel <neelotpal10@gmail.com>
2025-08-17 15:15:59 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,5 @@
+const heading=React.createElement("h1",{id: "heading"},"Hello World from React!");
+const heading2=React.createElement("h2",{id: "heading2"},"This is a second heading");
+const container=React.createElement("div",{id: "container"},[heading, heading2]);
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
